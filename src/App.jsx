9e69d0ab65aef8cdf2471ffccaa0a214f1a85590
dangmatong <tienkiem1801@gmail.com>
2024-11-23 12:53:21 +0700

    add font family

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AppRoutes from "./routes/AppRoutes";
 import { StoreProvider } from "./utils/store";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
   return (
