62e14c3a5f24c5c603a8a01f1fdaddf64e2e4a38
dangmatong <tienkiem1801@gmail.com>
2024-11-29 20:43:21 +0700

    fix blur layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import "./Layout.css";
 const Layout = ({ children }) => {
   return (
     <div id="layout" className="relative h-screen w-screen overflow-y-auto">
-      <div className="overlay absolute inset-0">
+      <div className="overlay absolute w-full">
         <div className="bg-container container py-5">
           <div className="bg-main min-h-[90vh] pt-2 py-5 md:p-5 rounded-md shadow-md">
             <Header></Header>
