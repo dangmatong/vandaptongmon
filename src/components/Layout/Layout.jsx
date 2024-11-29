49b819bbb083e483a4b9f21d76ca7e3c92d16fc2
dangmatong <tienkiem1801@gmail.com>
2024-11-29 18:41:38 +0700

    blur bg layout'

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,11 +3,13 @@ import "./Layout.css";
 
 const Layout = ({ children }) => {
   return (
-    <div id="layout" className="h-screen w-screen overflow-y-auto">
-      <div className="container py-5">
-        <div className="bg-main min-h-[90vh] pt-2 py-5 md:p-5 rounded-md shadow-md">
-          <Header></Header>
-          <main>{children}</main>
+    <div id="layout" className="relative h-screen w-screen overflow-y-auto">
+      <div className="overlay absolute inset-0">
+        <div className="bg-container container py-5">
+          <div className="bg-main min-h-[90vh] pt-2 py-5 md:p-5 rounded-md shadow-md">
+            <Header></Header>
+            <main>{children}</main>
+          </div>
         </div>
       </div>
     </div>
